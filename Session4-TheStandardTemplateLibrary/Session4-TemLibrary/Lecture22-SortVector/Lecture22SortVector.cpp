2ae076bed2857fc3c847c43925d862912f3ebf06
Nguyen Minh Phuong <thusinhtk@gmail.com>
2017-12-08 23:48:08 +0700

    [Section4][Lecture22] Practices about Sorting vector, operator and setting operator for manually

---
+++ Session4-TheStandardTemplateLibrary/Session4-TemLibrary/Lecture22-SortVector/Lecture22SortVector.cpp
@@ -0,0 +1,61 @@
+// Lecture22SortVector.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <vector> //vector
+#include <iostream> //
+#include <string> //cout
+#include <algorithm> //sort
+using namespace std;
+
+class Test {
+private:
+	int id;
+	string name;
+
+public:
+	Test(int id, string name) : id(id), name(name) {}
+
+	void print()
+	{
+		cout << id << " - " << name << endl;
+	}
+
+	/*bool operator < (const Test &other) const {
+		if (id == other.id)
+			return name < other.name;
+		else
+			return id < other.id;
+	}*/
+
+	friend bool comp(const Test &first, const Test &second) {
+		if (first.id == second.id) {
+			return first.name < second.name;
+		}
+		else
+			return first.id < second.id;
+	}
+};
+
+
+
+int main()
+{
+	vector<Test> vectors;
+
+	vectors.push_back(Test(10, "Lan"));
+	vectors.push_back(Test(12, "Hoa"));
+	vectors.push_back(Test(11, "Hue"));
+	vectors.push_back(Test(8, "Nhan"));
+	vectors.push_back(Test(20, "Thinh"));
+
+	//Bsort(vectors.begin(), vectors.end()); // call operator
+
+	sort(vectors.begin(),vectors.end(),comp);
+
+	for (int i = 0; i < vectors.size(); i++) {
+		vectors[i].print();
+	}
+    return 0;
+}
+
