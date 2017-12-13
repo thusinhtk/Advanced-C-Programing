c006fcfaa9ffaf69f704bba6be081a5d22b92ebb
Nguyen Minh Phuong <thusinhtk@gmail.com>
2017-12-13 23:40:03 +0700

    [Section5][Lecture29] Practices about operator *, Happy coding :))

---
+++ Section5-Overloading the Assignment Operator/Section5-OperatorOverloading/29.OverloadingTheDereferenceOperator/29.OverloadingTheDereferenceOperator.cpp
@@ -0,0 +1,16 @@
+// 29OverloadingTheDereferenceOperator.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "Complex29.h"
+
+using namespace caveofprogramming;
+
+int main()
+{
+	Complex c1(4, 4);
+	cout << *c1<< endl;
+
+    return 0;
+}
+
---
+++ Section5-Overloading the Assignment Operator/Section5-OperatorOverloading/29.OverloadingTheDereferenceOperator/Complex29.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+namespace caveofprogramming
+{
+	class Complex
+	{
+	private:
+		int real;
+		int imaginary;
+	public:
+		Complex();
+		~Complex();
+		Complex(int real, int imaginary);
+		Complex(const Complex &other);
+		const Complex &operator=(const Complex &other);
+
+		int getReal() const
+		{
+			return real;
+		}
+		int getImaginary() const
+		{
+			return imaginary;
+		}
+		bool operator==(const Complex &other) const;
+		bool operator!=(const Complex &other) const;
+
+		Complex operator*() const;
+
+	};
+
+	ostream &operator<<(ostream &out, const Complex &other);
+	Complex &operator+(const Complex &c1, const Complex &c2);
+	Complex &operator+(const Complex &c1, int numberToPlus);
+	Complex &operator+(int numberToPlus, const Complex &c1);
+
+} /* caveofprogramming */
+
