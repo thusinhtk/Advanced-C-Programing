168f86c0c741140f42d94ad12d95234170dbe075
Nguyen Minh Phuong <thusinhtk@gmail.com>
2018-01-16 23:14:46 +0700

    [Section8][Lecture58] Rvalues and lvalues

---
+++ Section8-C++11NewFeatures/58.RvaluesAndLvalues/58RvaluesAndLvalues.cpp
@@ -0,0 +1,96 @@
+// 58RvaluesAndLvalues.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <memory.h>
+
+using namespace std;
+
+class Test
+{
+private:
+	static const int SIZE = 100;
+	int* _pBuffer;
+public:
+	Test()
+	{
+		cout << "Constructor" << endl;
+
+		_pBuffer = new int[SIZE] {};
+	}
+
+	Test(int i)
+	{
+		cout << "Parameterized" << endl;
+
+		_pBuffer = new int [SIZE] {};
+
+		for (unsigned i = 0; i < SIZE; ++i)
+		{
+			_pBuffer[i] = 7 * i;
+		}
+	}
+
+	Test(const Test &other)
+	{
+		cout << "Copy constructor" << endl;
+
+		_pBuffer = new int [SIZE] {};
+		memcpy(_pBuffer, other._pBuffer, SIZE * sizeof(int));
+	}
+
+	Test &operator=(const Test &other)
+	{
+		cout << "asssigment" << endl;
+
+		_pBuffer = new int [SIZE] {};
+		memcpy(_pBuffer, other._pBuffer, SIZE * sizeof(int));
+
+		return *this;
+	}
+
+	~Test()
+	{
+		cout << "Deconstructor" << endl;
+	}
+
+	friend ostream &operator<<(ostream &out, const Test &other);
+};
+
+ostream &operator<<(ostream &out, const Test &other)
+{
+	out << "Hello from test";
+	return out;
+}
+
+Test getTest()
+{
+	return Test();
+}
+
+int main()
+{
+	Test test1 = getTest();
+
+	cout << test1 << endl;
+
+	vector<Test> vec;
+	vec.push_back(Test());
+
+	int value1 = 7;
+
+	int *pValue = &value1;
+	//int *pValue2 = &7;
+
+	Test *pTest1 = &test1;
+	//Test *pTest2 = &getTest();
+
+	int *pValue3 = &++value1;
+	//int *pValue4 = &value1++;
+	cout << *pValue3 << endl;
+
+	return 0;
+}
